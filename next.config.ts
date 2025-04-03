4b1045ccf32517f6847e20c8f3623202f4d34058
nashmonzon <ignmonzon@gmail.com>
2025-04-03 13:30:57 +0200

    Added all screen

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
+import type { Configuration } from "webpack";
+import type { NextConfig } from "next";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    // Configuración simplificada para Lit
-    config.module.rules.push({
+const nextConfig: NextConfig = {
+  webpack: (config: Configuration) => {
+    config.module?.rules?.push({
       test: /\.m?js$/,
       include: /node_modules\/lit-element|lit-html|@lit|lit/,
       resolve: {
